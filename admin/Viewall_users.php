78997e8bbc5591e9939324564ce03ef115d5e4fc
nashaatnabil0 <nashaatnabil@live.com>
2024-09-12 00:18:37 +0300

    handeling view all & delete users

--- admin/Viewall_users.php
+++ admin/Viewall_users.php
@@ -9,16 +9,12 @@ if (strlen($_SESSION['sportadmission']==0)) {
 if(isset($_GET['delid']))
 {
 $rid=intval($_GET['delid']);
-$delete_image = $pdoConnection -> query("select Profilepic from tblartist where ID='$rid'");
-$image_data = $delete_image-> fetch(PDO:: FETCH_ASSOC);
-$image_name = $image_data['Profilepic'];
 
-$sql= $pdoConnection -> query("DELETE FROM tblartist WHERE ID='$rid'");
+$sql= $pdoConnection -> query("DELETE FROM payment WHERE ID='$rid'");
 
   if($sql){
-    unlink("images/$image_name");
     echo "<script>alert('Data deleted');</script>"; 
-    echo "<script>window.location.href = 'manage-artist.php'</script>";     
+    echo "<script>window.location.href = 'Viewall_users.php'</script>";     
   }
 }
 ?>
@@ -58,11 +54,11 @@ $sql= $pdoConnection -> query("DELETE FROM tblartist WHERE ID='$rid'");
       <section class="wrapper">
         <div class="row">
           <div class="col-lg-12">
-            <h3 class="page-header"><i class="fa fa-table"></i> Manage Artist</h3>
+            <h3 class="page-header"><i class="fa fa-table"></i>User Management</h3>
             <ol class="breadcrumb">
               <li><i class="fa fa-home"></i><a href="dashboard.php">Home</a></li>
-              <li><i class="fa fa-table"></i>Artist</li>
-              <li><i class="fa fa-th-list"></i>Manage Artist</li>
+              <li><i class="fa fa-table"></i>Users</li>
+              <li><i class="fa fa-th-list"></i>View all Users</li>
             </ol>
           </div>
         </div>
@@ -71,32 +67,32 @@ $sql= $pdoConnection -> query("DELETE FROM tblartist WHERE ID='$rid'");
           <div class="col-sm-12">
             <section class="panel">
               <header class="panel-heading">
-                Manage Artist
+                Users Search
+                <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search by name, email, mobile number, or role..." class="form-control mb-3" style="margin-bottom: 10px;">
               </header>
               <table class="table">
-                <thead>
-                                        
+                <thead>                     
                   <tr>
                     <th>S.NO</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Mobile Number</th>
-                    <th>Registration Date</th>
+                    <th>Role</th>
                     <th>Action</th>
                   </tr>
                 </thead>
                 <?php
-                  $ret= $pdoConnection-> query("SELECT * FROM tblartist");
+                  $ret= $pdoConnection-> query("SELECT * FROM users");
                   $cnt=1;
                   while ($row=$ret-> fetch(PDO:: FETCH_ASSOC)) {
                 ?>
                 <tr>
                   <td><?php echo $cnt;?></td>
-                  <td><?php  echo $row['Name'];?></td>
+                  <td><?php  echo $row['name'];?></td>
                   <td><?php  echo $row['Email'];?></td> 
                   <td><?php  echo $row['MobileNumber'];?></td>
-                  <td><?php  echo $row['CreationDate'];?></td>
-                  <td><a href="edit-artist-detail.php?editid=<?php echo $row['ID'];?>" class="btn btn-success">Edit</a> || <a href="manage-artist.php?delid=<?php echo $row['ID'];?>" class="btn btn-danger confirm">Delete</a></td>
+                  <td><?php  echo $row['role'];?></td>
+                  <td><a href="edit_user_detail.php?editid=<?php echo $row['ID'];?>" class="btn btn-success">Edit</a> || <a href="Viewall_users.php?delid=<?php echo $row['ID'];?>" class="btn btn-danger confirm">Delete</a></td>
                 </tr>
                 <?php 
                   $cnt=$cnt+1;
@@ -104,9 +100,7 @@ $sql= $pdoConnection -> query("DELETE FROM tblartist WHERE ID='$rid'");
               </table>
             </section>
           </div>
-       
         </div>
-       
         <!-- page end-->
       </section>
     </section>
@@ -122,17 +116,54 @@ $sql= $pdoConnection -> query("DELETE FROM tblartist WHERE ID='$rid'");
   <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
   <!--custome script for all page-->
   <script src="js/scripts.js"></script>
-<script>
-  let deleteBtn = document.querySelectorAll(".confirm");
-for (let i = 0; i < deleteBtn.length; i++) {
-    deleteBtn[i].addEventListener("click", (e) => {
-        let ans = confirm("Are You Sure!!")
-        if (!ans) {
-            e.preventDefault();
-        }
-    })
-}
-</script>
+  <!-- confirm script -->
+  <script>
+    let deleteBtn = document.querySelectorAll(".confirm");
+    for (let i = 0; i < deleteBtn.length; i++) {
+        deleteBtn[i].addEventListener("click", (e) => {
+            let ans = confirm("Are You Sure!!")
+            if (!ans) {
+                e.preventDefault();
+            }
+        })
+    }
+  </script>
+  <!-- search script -->
+  <script>
+    function searchTable() {
+      var input = document.getElementById("searchInput");
+      var filter = input.value.toUpperCase();
+      var table = document.querySelector(".table");
+      var tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows, excluding the header (index 0)
+      for (var i = 1; i < tr.length; i++) {
+        var tdName = tr[i].getElementsByTagName("td")[1]; // Name column
+        var tdEmail = tr[i].getElementsByTagName("td")[2]; // Email column
+        var tdMobile = tr[i].getElementsByTagName("td")[3]; // Mobile Number column
+        var tdRole = tr[i].getElementsByTagName("td")[4]; // Role column
+        
+        if (tdName || tdEmail || tdMobile || tdRole) {
+          var nameText = tdName.textContent || tdName.innerText;
+          var emailText = tdEmail.textContent || tdEmail.innerText;
+          var mobileText = tdMobile.textContent || tdMobile.innerText;
+          var roleText = tdRole.textContent || tdRole.innerText;
+
+          // Check if any column matches the search query
+          if (
+            nameText.toUpperCase().indexOf(filter) > -1 || 
+            emailText.toUpperCase().indexOf(filter) > -1 || 
+            mobileText.toUpperCase().indexOf(filter) > -1 || 
+            roleText.toUpperCase().indexOf(filter) > -1
+          ) {
+            tr[i].style.display = ""; // Show the row if it matches
+          } else {
+            tr[i].style.display = "none"; // Hide the row if it doesn't match
+          }
+        }       
+      }
+    }
+  </script>
 
 </body>
 
