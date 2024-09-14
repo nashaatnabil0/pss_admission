2add9bb738cdda5b6de5d39870c6146d15195615
samahAhmed <samah.ahmed.0012@gmail.com>
2024-09-15 02:30:32 +0300

    edit edit_enrollment_detail error in line 54

--- admin/edit_enrollment_detail.php
+++ admin/edit_enrollment_detail.php
@@ -1,30 +1,67 @@
 <?php
 session_start();
-error_reporting(0);
+//error_reporting(0);
 include('includes/dbconnection.php');
 if (strlen($_SESSION['sportadmission']==0)) {
   header('location:logout.php');
   }
-  else{
+else {  
+  $errors = [];
+  $cid=$_GET['editid'];
 
-if(isset($_POST['submit']))
-  {
-    
-    $arttype=$_POST['arttype'];
-    
-  $eid=$_GET['editid'];
-  $query=$pdoConnection-> query("update tblarttype set ArtType='$arttype' where ID='$eid'");
-    if ($query) {
-      echo "<script>alert('Art type has been updated.');  location.href='manage-art-type.php'</script>";
-  }
-  else
-    {
-      echo "<script>alert('Something Went Wrong. Please try again.');</script>";
+   // Fetch all trainee NIDs from the database
+   $stmt = $pdoConnection->query("SELECT NID, Name FROM trainees");
+   $trainees = $stmt->fetchAll(PDO::FETCH_ASSOC); // Fetch all NIDs and names
+
+  if(isset($_POST['submit'])){
+    $traineeNID = $_POST['traineeNID'];
+    // Validate traineeNID
+    // Check if the trainee exists in the 'trainees' table and has no existing enrollment
+    $stmt = $pdoConnection->query(" SELECT COUNT(*) FROM trainees 
+                                      LEFT JOIN enrollment ON trainees.NID = enrollment.traineeNID 
+                                      WHERE trainees.NID = $traineeNID AND (enrollment.state IS NULL OR enrollment.state IN ('off', 'waiting'))");
+    $exists = $stmt->fetchColumn();
+
+    $group = $_POST['group'];
+    if (empty($group)) {
+      $errors['group'] = "Please select a group";
     }
 
-  }
+    $pymntPlan = $_POST['pymntplan'];
+    if (empty($pymntPlan)) {
+      $errors['pymntplan'] = "Please select a payment plan";
+    }
 
-  ?>
+    $enrollstate = $_POST['enrollstate'];
+    if (empty($enrollstate)) {
+      $errors['enrollstate'] = "Please select an enrollment state";
+    }
+
+    $enrolldate = $_POST['enrolldate'];
+    if ($enrolldate == "") {
+        $enrolldate = date('Y-m-d') ;
+    }else{
+      $enrolldate = $_POST['enrolldate'];
+    }
+    
+    $discount = $_POST['discount'];
+    if ($discount == "") {
+      $discount = null;
+    }
+    
+    if (empty($errors)){
+      
+    $query2 = $pdoConnection->query("UPDATE enrollment SET groupId ='$group', paymentPlan'= $pymntPlan', state='$enrollstate', date='$enrolldate', discount='$discount' where ID = '$cid';");
+    
+          if ($query2) {
+              echo "<script>alert('Enrollment details have been updated.');</script>";
+              echo "<script>window.location.href ='viewall_enrollments.php'</script>";
+          } else {
+              echo "<script>alert('Something Went Wrong. Please try again.');</script>";
+            }
+      }
+ }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -70,11 +107,11 @@ if(isset($_POST['submit']))
       <section class="wrapper">
         <div class="row">
           <div class="col-lg-12">
-            <h3 class="page-header"><i class="fa fa-file-text-o"></i>Update Art Type Detail</h3>
+            <h3 class="page-header"><i class="fa fa-file-text-o"></i>Update Enrollment Details</h3>
             <ol class="breadcrumb">
               <li><i class="fa fa-home"></i><a href="dashboard.php">Home</a></li>
-              <li><i class="icon_document_alt"></i>Update Art Type</li>
-              <li><i class="fa fa-file-text-o"></i>Update Art Type Detail</li>
+              <li><i class="icon_document_alt"></i>Update Enrollment</li>
+              <li><i class="fa fa-file-text-o"></i>Update Enrollment Details</li>
             </ol>
           </div>
         </div>
@@ -82,33 +119,90 @@ if(isset($_POST['submit']))
           <div class="col-lg-12">
             <section class="panel">
               <header class="panel-heading">
-               Update Art Type Detail
+               Update Enrollment Details
               </header>
               <div class="panel-body">
-                <form class="form-horizontal " method="post" action="">
-                 
-
-  <?php
- $cid=$_GET['editid'];
-
-$query=$pdoConnection-> query("select * from tblarttype where ID='$cid'");
-     $cnt=1;
-while ($row=$query ->fetch(PDO:: FETCH_ASSOC)) {
-
-?>
+              <form class="form-horizontal " method="post" action="" enctype="multipart/form-data" novalidate>
+              <?php
+              $query=$pdoConnection-> query("select * from enrollment where ID='$cid'");
+                      $row2=$query ->fetch(PDO:: FETCH_ASSOC);
+                      if($row2 > 0) {
+                      ?>  
+              <!-- Trainee National ID input with datalist -->
+                <div class="form-group">
+                <label class="col-sm-2 control-label">Trainee National ID</label>
+                <div class="col-sm-10">
+                  <input class="form-control" id="searchTrainee" name="traineeNID" type="text" value="<?php  echo $row2['traineeNID']; ?>" placeholder="Type the national ID number" list="traineeListOptions" readonly/>
+                  <datalist id="traineeListOptions">
+                    <?php foreach ($trainees as $trainee) { ?>
+                      <option value="<?php echo $trainee['NID']; ?>">
+                        <?php echo $trainee['NID'] . " - " . $trainee['Name']; ?>
+                      </option>
+                    <?php } ?>
+                  </datalist>
+                  <?php if (isset($_POST['submit']) && isset($errors['traineeNID'])){ ?>
+                    <span style="color:red;display:block;text-align:left"><?php echo $errors['traineeNID']; ?></span>
+                  <?php } ?>
+                  <?php if (isset($_POST['submit']) && isset($errors['traineeNIDinvalid'])) { ?>
+                    <span style="color:red;display:block;text-align:left"><?php echo $errors['traineeNIDinvalid']; ?></span>
+                  <?php } ?>
+                </div>
+                </div>
                   <div class="form-group">
-                    <label class="col-sm-2 control-label">Art Type</label>
+                    <label class="col-sm-2 control-label">Group</label>
                     <div class="col-sm-10">
-                      <input class="form-control" id="arttype" name="arttype"  type="text" required="true" value="<?php  echo $row['ArtType'];?>">
+                      <select class="form-control m-bot15" name="group" id="group">
+                        <option value="" >Choose a group</option>
+                          <?php $query=$pdoConnection-> query("select * from groups");
+                            while($row=$query ->fetch(PDO:: FETCH_ASSOC))
+                            {
+                            ?> 
+                          <option value="<?php echo $row['ID'];?>" <?php if($row2['groupId'] == $row['ID']){ echo "selected='selected'";}?> ><?php echo $row['Title'];?></option>
+                            <?php } ?> 
+                      </select>
+                      <?php if (isset($_POST['submit']) && isset($errors['group'])){ ?>
+                    <span style="color:red;display:block;text-align:left"><?php echo $errors['group']; ?></span>
+                  <?php } ?>
                     </div>
                   </div>
-                   
-                <?php } ?>
-                 <p style="text-align: center;"> <button type="submit" name='submit' class="btn btn-primary">Update</button></p>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">Payment Plan</label>
+                    <div class="col-sm-10">
+                      <input class="" id="paymentPlan" name="pymntplan"  type="radio" value="full" <?php if($row2['paymentPlan'] === 'full') {echo 'checked';} ?>  style="margin:7px" required> Full <span style="margin: 35px"></span>
+                      <input class="" id="paymentPlan" name="pymntplan"  type="radio" value="installment" <?php if($row2 ['paymentPlan'] === 'installment') {echo 'checked';} ?> style="margin:7px" required > Installments
+                     <?php if (isset($_POST['submit']) && isset($errors['pymntplan'])){ ?>
+                     <span style="color:red;display:block;text-align:left"><?php echo $errors['pymntplan']; ?></span>
+                     <?php } ?>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">Discount</label>
+                    <div class="col-sm-10">
+                      <input class="form-control" id="discount" name="discount"  type="text" value="<?php echo $row2['discount'] === null ? 0 : $row2['discount']; ?>">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">Enrollment State:</label>
+                    <div class="col-sm-10">
+                    <input class="" id="enrolltate" name="enrollstate"  type="radio" value="on" <?php if($row2['state']==='on'){echo 'checked';} ?> style="margin:7px" required > Active <span style="margin: 35px"></span>
+                    <input class="" id="enrollstateoff" name="enrollstate"  type="radio" value="off" <?php if($row2['state']==='off'){echo 'checked';} ?> style="margin:7px" required> Inactive  <span style="margin: 35px"></span>
+                    <input class="" id="enrollstatewait" name="enrollstate"  type="radio" value="waiting" <?php if($row2['state']==='waiting'){echo 'checked';} ?> style="margin:7px" required> Waiting   
+                    <?php if (isset($_POST['submit']) && isset($errors['enrollstate'])){ ?>
+                    <span style="color:red;display:block;text-align:left"><?php echo $errors['enrollstate']; ?></span>
+                  <?php } ?>
+                  </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label">Enrollment Date</label>
+                    <div class="col-sm-10">
+                      <input class="form-control" id="enrolldate" name="enrolldate" type="date" value="<?php echo $row2['date']?>">
+                      </div>
+                    </div>
+                 <p style="text-align: center;"> <button type="submit" name="submit" class="btn btn-primary">Submit</button></p>
                 </form>
+                <?php } ?>
               </div>
-            </section>
-            
+            </section> 
           </div>
         </div>
         <!-- Basic Forms & Horizontal Forms-->
