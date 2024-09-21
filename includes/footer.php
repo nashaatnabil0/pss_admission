aef3d63e35d5607538faf365a2847c11619c3013
AnneOsama01 <annosama7801@gmail.com>
2024-09-21 13:31:27 +0300

    remove unnecessary div from footer

--- includes/footer.php
+++ includes/footer.php
@@ -40,8 +40,8 @@
             </div> -->
         </div>
     </div>
-    <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: #3E3E4E !important;">
-        <div class="row">
+    <!-- <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: #3E3E4E !important;">
+        <div class="row"> -->
             <!-- <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
                 <p class="m-0 text-white">&copy; <a href="#">Your Site Name</a>. All Rights Reserved. 
 				 -->
